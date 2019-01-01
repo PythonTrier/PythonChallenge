33cb833d659f4021ddebdebaeeaf9b5977789c98
Steglitz <steglitz@pm.me>
2019-01-02 00:49:04 +0100

    Correct Python7 by updating code and merge Python8 as it's actuall part two of Python7.

--- Python7.py
+++ Python7.py
@@ -1,16 +1,22 @@
 from PIL import Image
-import urllib.request
-
-base_url = "http://www.pythonchallenge.com/pc/def/oxygen.html"
-
-# Download File
-x = urllib.request.urlopen(base_url)
+import re
 
 img = Image.open('oxygen.png')
 pxbuffer = img.load()
 
 output = ""
-for x in range(2, 609, 7) :
-    output += chr(pxbuffer[x, 47][0])
+output2 = ""
+for x in range(1, img.width, 7):
+    output += chr(pxbuffer[x, img.height/2][0])
 
 print(output)
+
+numbers = re.findall("\d+", output)
+
+print(numbers)
+for x in numbers:
+    output2 += (chr(int(x)))
+
+print(output2)
+
+# integrity
